3cb06f984e5535bb75f295a6886044b99a6254d6
Vinícius Fernandes <vinicius.fernandes.abreu120@gmail.com>
2026-02-28 21:34:08 -0300

    Add files via upload

---
+++ Nivel_facil/ex.21-02-2026.py
@@ -0,0 +1,12 @@
+# Nível Fácil
+
+# São fornecidos dois números como entrada, num1 e num2.
+# Sua tarefa é calcular o produto deles e armazenar o resultado em uma variável chamada product.
+# Verifique os casos de teste para ver as entradas e as saídas esperadas.
+
+# Resolução
+
+num1 = int(input()) # Don't change this line
+num2 = int(input()) # Don't change this line
+product = num1 * num2 
+print(f"product = {product}") # Don't change this line
