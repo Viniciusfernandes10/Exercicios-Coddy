b7ce83c4556b46a10f9d3f71c43ca2dfc11eb15c
Vinícius Fernandes <vinicius.fernandes.abreu120@gmail.com>
2026-03-18 22:10:00 -0300

    Add files via upload

---
+++ Nivel_facil/ex.09-03-2026.py
@@ -0,0 +1,11 @@
+# # Nível Fácil
+
+# Armazene uma entrada de string em input_string.
+
+# Crie e imprima uma nova string: "Vexillate says: " seguido por input_string.
+
+
+# # Resolução
+
+input_string = input() # Don't change this line
+print(f"Vexillate says: {input_string}")
