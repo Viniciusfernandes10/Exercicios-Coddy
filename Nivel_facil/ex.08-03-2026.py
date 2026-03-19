d104634ea0eb6408eeff8e2e3d88becd110544d9
Vinícius Fernandes <vinicius.fernandes.abreu120@gmail.com>
2026-03-18 22:09:38 -0300

    Add files via upload

---
+++ Nivel_facil/ex.08-03-2026.py
@@ -0,0 +1,23 @@
+# # Nível Fácil
+
+# Crie uma função chamada create_plant_report que recebe plant_name e height como seus parâmetros.
+
+# Como botânico em um laboratório universitário, você precisa criar um relatório simples de crescimento de plantas para seus registros de laboratório. 
+# Sua tarefa é combinar o nome da planta e sua altura atual em uma string formatada.
+
+# Parâmetros:
+
+# plant_name (str): O nome da planta sendo estudada.
+# height (int): A altura atual da planta em centímetros.
+# A função deve retornar uma string que combina o nome da planta e a altura em uma mensagem de relatório formatada. 
+# A mensagem deve estar no seguinte formato: "The [plant_name] plant has grown to [height] cm tall."
+
+
+# # Resolução
+
+def create_plant_report(plant_name, height):
+    # Write code here
+    return f"The {plant_name} plant has grown to {height} cm tall."
+
+
+
